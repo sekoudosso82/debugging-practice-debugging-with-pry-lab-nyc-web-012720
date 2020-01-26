e4500a0ea12feba59c9cda86748c60b3eb55c128
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-26 06:18:59 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,6 +1,9 @@
+require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-  10 * "s" + string
+  p ("s"*10 + string)
+  # binding.pry
+   
   else
   string
   end
--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -12,11 +14,12 @@ def crew
 end
 
 def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+  crew.each {|crew_member| puts "Hello #{crew_member}."}
 end
 
 def engage
   date = generate_star_date
   state_log(date)
   greet_crew(crew)
+  # binding.pry 
 end
